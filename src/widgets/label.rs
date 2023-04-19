d55259a5cae4ba863b12f1b3879adf068e1d8106
jtnunley <jtnunley01@gmail.com>
2023-04-19 14:07:03 -0700

    Use AGPL

--- src/widgets/label.rs
+++ src/widgets/label.rs
@@ -1,20 +1,21 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later OR MPL-2.0
-// This file is a part of `sunder`.
-//
-// `sunder` is free software: you can redistribute it and/or modify it under the terms of
-// either:
-//
-// * GNU Lesser General Public License as published by the Free Software Foundation, either
-// version 3 of the License, or (at your option) any later version.
-// * Mozilla Public License as published by the Mozilla Foundation, version 2.
-//
-// `sunder` is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU Lesser General Public License or the Mozilla Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License and the Mozilla
-// Public License along with `sunder`. If not, see <https://www.gnu.org/licenses/> or
-// <https://www.mozilla.org/en-US/MPL/2.0/>.
+/*
+
+`sunder` is free software: you can redistribute it and/or modify it under the terms of one of
+the following licenses:
+
+- The GNU Affero General Public License as published by the Free Software Foundation, either version
+  3 of the License, or (at your option) any later version.
+- The Patron License at https://github.com/notgull/sunder/blob/main/LICENSE-PATRON.md, for
+  sponsors and contributors, who can ignore the copyleft provisions of the GNU AGPL for this project.
+
+`sunder` is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
+Public License and the Patron License for more details.
+
+You should have received a copy of the GNU Affero General Public License and the corresponding Patron
+License along with `sunder`. If not, see <https://www.gnu.org/licenses/>.
+
+*/
 
 //! A text label.
 
